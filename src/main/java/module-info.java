42eeea4232f23fb3c20546008f72149ec2de5f1a
ElliottAndCoachGeorge <edshaver@thewildcats.net>
2026-05-08 11:56:30 -0400

    Fixing error in module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module com.elliottandcoachgeorge.javafxtest {
+
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens com.elliottandcoachgeorge.javafxtest to javafx.fxml;
+
     exports com.elliottandcoachgeorge.javafxtest;
 }
